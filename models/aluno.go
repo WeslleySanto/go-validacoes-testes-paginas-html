0e937de9c41ed292b5cdfc161e7da73384612c23
Weslley Santo <weslley.santo@gmail.com>
2023-05-14 14:03:46 -0300

    Finalizando aula 01. Instalando e criando a primeira rota com Gin

--- models/aluno.go
+++ models/aluno.go
@@ -1,10 +1,20 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gopkg.in/validator.v2"
+	"gorm.io/gorm"
+)
 
 type Aluno struct {
 	gorm.Model
-	Nome string `json:"nome"`
-	CPF  string `json:"cpf"`
-	RG   string `json:"rg"`
+	Nome string `json:"nome" validate:"nonzero"`
+	RG   string `json:"rg" validate:"len=9"`
+	CPF  string `json:"cpf" validate:"len=11, regexp=^[0-9]*$"`
+}
+
+func ValidaAluno(a *Aluno) error {
+	if err := validator.Validate(a); err != nil {
+		return err
+	}
+	return nil
 }
